40aa915ca24efa7524cce7d519fdfd604d96bc0a
SampreetG22 <sampreet.g.45@gmail.com>
2023-04-29 13:32:14 +0000

    Nxt Trends 2

---
+++ src/components/Header/index.js
@@ -0,0 +1,85 @@
+import Cookies from 'js-cookie'
+import {Link, withRouter} from 'react-router-dom'
+import './index.css'
+
+const Header = props => {
+  const {history} = props
+  const logout = () => {
+    Cookies.remove('jwt_token')
+    history.replace('/login')
+  }
+
+  return (
+    <nav className="nav-header">
+      <div className="nav-content">
+        <div className="nav-bar-mobile-logo-container">
+          <img
+            className="website-logo"
+            src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-logo-img.png"
+            alt="website logo"
+          />
+
+          <button type="button" className="nav-mobile-btn">
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-log-out-img.png"
+              alt="nav logout"
+              className="nav-bar-img"
+            />
+          </button>
+        </div>
+
+        <div className="nav-content nav-bar-large-container">
+          <img
+            className="website-logo"
+            src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-logo-img.png"
+            alt="website logo"
+          />
+          <ul className="nav-menu">
+            <Link to="/" className="linksCSS">
+              <li className="nav-menu-item">Home</li>
+            </Link>
+
+            <Link to="/products" className="linksCSS">
+              <li className="nav-menu-item">Products</li>
+            </Link>
+
+            <Link to="/cart" className="linksCSS">
+              <li className="nav-menu-item">Cart</li>
+            </Link>
+          </ul>
+          <button type="button" className="logout-desktop-btn" onClick={logout}>
+            Logout
+          </button>
+        </div>
+      </div>
+      <div className="nav-menu-mobile">
+        <ul className="nav-menu-list-mobile">
+          <li className="nav-menu-item-mobile">
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-home-icon.png"
+              alt="nav home"
+              className="nav-bar-img"
+            />
+          </li>
+
+          <li className="nav-menu-item-mobile">
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-icon.png"
+              alt="nav products"
+              className="nav-bar-img"
+            />
+          </li>
+
+          <li className="nav-menu-item-mobile">
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-icon.png"
+              alt="nav cart"
+              className="nav-bar-img"
+            />
+          </li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
+export default withRouter(Header)
